8087dc3786039336b9c23b1b188699516b8922ce
diegochoqueramirez <diegochoqueramirez@gmail.com>
2022-01-30 01:43:59 -0400

    styles ended

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,6 +4,7 @@ import {
   ProductImage,
   ProductTitle,
 } from "../components";
+import "../styles/custom-styles.css";
 
 const product = {
   id: "1",
@@ -22,16 +23,24 @@ export const ShoppingPage = () => {
           flexFlow: "row wrap",
         }}
       >
-        <ProductCard product={product}>
-          <ProductImage />
-          <ProductTitle title="asd" />
-          <ProductButtons />
-        </ProductCard>
-
-        <ProductCard product={product}>
+        <ProductCard product={product} className="bg-dark text-white">
           <ProductCard.Image />
           <ProductCard.Title />
-          <ProductCard.Buttons />
+          <ProductCard.Buttons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductImage className="custom-image" />
+          <ProductTitle className="text-bold" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard product={product} style={{ backgroundColor: "#70D1F8" }}>
+          <ProductImage
+            style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
+          />
+          <ProductTitle style={{ fontWeight: "bold" }} />
+          <ProductButtons style={{ display: "flex", justifyContent: "end" }} />
         </ProductCard>
       </div>
     </div>
